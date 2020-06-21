66b65ad73f39be81acddce231d879342011e1431
lucido1 <goolucido@gmail.com>
2020-06-21 11:42:28 +0800

    dev/test1 修改代码测试

--- tensquare_article/src/main/java/com/tensquare/article/pojo/Article.java
+++ tensquare_article/src/main/java/com/tensquare/article/pojo/Article.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 /**
- * 实体类
+ * Article实体类
  * @author Administrator
  *
  */
@@ -16,8 +16,6 @@ public class Article implements Serializable{
 	@Id
 	private String id;//ID
 
-
-	
 	private String columnid;//专栏ID
 	private String userid;//用户ID
 	private String title;//标题
@@ -35,78 +33,78 @@ public class Article implements Serializable{
 	private String url;//URL
 	private String type;//类型
 
-	
-	public String getId() {		
+
+	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
 
-	public String getColumnid() {		
+	public String getColumnid() {
 		return columnid;
 	}
 	public void setColumnid(String columnid) {
 		this.columnid = columnid;
 	}
 
-	public String getUserid() {		
+	public String getUserid() {
 		return userid;
 	}
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
 
-	public String getTitle() {		
+	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	public String getContent() {		
+	public String getContent() {
 		return content;
 	}
 	public void setContent(String content) {
 		this.content = content;
 	}
 
-	public String getImage() {		
+	public String getImage() {
 		return image;
 	}
 	public void setImage(String image) {
 		this.image = image;
 	}
 
-	public java.util.Date getCreatetime() {		
+	public java.util.Date getCreatetime() {
 		return createtime;
 	}
 	public void setCreatetime(java.util.Date createtime) {
 		this.createtime = createtime;
 	}
 
-	public java.util.Date getUpdatetime() {		
+	public java.util.Date getUpdatetime() {
 		return updatetime;
 	}
 	public void setUpdatetime(java.util.Date updatetime) {
 		this.updatetime = updatetime;
 	}
 
-	public String getIspublic() {		
+	public String getIspublic() {
 		return ispublic;
 	}
 	public void setIspublic(String ispublic) {
 		this.ispublic = ispublic;
 	}
 
-	public String getIstop() {		
+	public String getIstop() {
 		return istop;
 	}
 	public void setIstop(String istop) {
 		this.istop = istop;
 	}
 
-	public Integer getVisits() {		
+	public Integer getVisits() {
 		return visits;
 	}
 	public void setVisits(Integer visits) {
@@ -120,35 +118,35 @@ public class Article implements Serializable{
 		this.thumbup = thumbup;
 	}
 
-	public Integer getComment() {		
+	public Integer getComment() {
 		return comment;
 	}
 	public void setComment(Integer comment) {
 		this.comment = comment;
 	}
 
-	public String getState() {		
+	public String getState() {
 		return state;
 	}
 	public void setState(String state) {
 		this.state = state;
 	}
 
-	public String getChannelid() {		
+	public String getChannelid() {
 		return channelid;
 	}
 	public void setChannelid(String channelid) {
 		this.channelid = channelid;
 	}
 
-	public String getUrl() {		
+	public String getUrl() {
 		return url;
 	}
 	public void setUrl(String url) {
 		this.url = url;
 	}
 
-	public String getType() {		
+	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
